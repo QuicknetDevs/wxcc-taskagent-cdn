a150ace3424024e1d29e6f75b2f36cda7218b407
QuicknetDevs <canalesdigitales@quicknet.com>
2025-08-28 13:10:15 -0600

    Update wxcc-taskagentauto.js

--- wxcc-taskagentauto.js
+++ wxcc-taskagentauto.js
@@ -1,46 +1,40 @@
 // wxcc-taskagentauto.js
 
-// Registramos el componente global con el mismo nombre que "comp" en DynamicArea
+// Asegúrate de que Webex esté disponible desde CDN (solo si quieres un SDK independiente)
+// <script src="https://unpkg.com/@webex/contact-center@next/umd/contact-center.min.js"></script>
+
 window['wxcc-taskagentauto'] = {
   init: (options) => {
     console.log('wxcc-taskagentauto init', options);
 
-    // Obtenemos el módulo Contact Center desde el Desktop nativo
+    // Usar el SDK ya cargado por Desktop
     const cc = window.Desktop?.cc;
     if (!cc) {
       console.error('Contact Center SDK no disponible en Desktop');
       return;
     }
 
-    // Listener para tareas entrantes
     cc.on('task:incoming', (task) => {
       console.log('Incoming task:', task);
-
-      // Ejemplo: aceptar la tarea automáticamente
-      task.accept()
-        .then(() => console.log('Task accepted automatically'))
-        .catch(err => console.error('Error accepting task:', err));
+      task.accept().then(() => console.log('Task accepted'));
     });
 
-    // Listener para tareas hidratadas (reload o relogin)
     cc.on('task:hydrate', (task) => {
       console.log('Task hydrated:', task);
     });
 
-    // Listener para tareas asignadas al agente
     cc.on('task:assigned', (task) => {
       console.log('Task assigned:', task.data.interactionId);
     });
 
-    // Retornamos el objeto destroy() para cuando se cierre el widget
     return {
       destroy: () => {
-        console.log('wxcc-taskagentauto destroyed');
-
         cc.off('task:incoming');
         cc.off('task:hydrate');
         cc.off('task:assigned');
+        console.log('wxcc-taskagentauto destroyed');
       }
     };
   }
 };
+
